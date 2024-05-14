21c5efcef09461dec276610fcccb8d29ad6d5293
arko92 <arkobhattacharjee92@gmail.com>
2024-05-14 21:40:36 +0200

    route for get_cars

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
     path(route='register', view=views.register_user, name='register'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
