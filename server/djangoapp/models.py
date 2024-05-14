74db265a97f1883524fd31b0e87b1d8aea640c73
arko92 <arkobhattacharjee92@gmail.com>
2024-05-14 20:32:58 +0200

    CarModel django model added

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,10 +1,33 @@
 
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('Wagon', 'Wagon')
+    ]
+    type = models.CharField(choices=CAR_TYPES, max_length=10, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
 
     def __str__(self) -> str:
         return self.name
