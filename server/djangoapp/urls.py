06b586cfa3153b136c10ddbda0514ea4206af875
arko92 <arkobhattacharjee92@gmail.com>
2024-05-20 10:18:16 +0200

    route and path added for delear details request

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,11 +7,13 @@ from django.conf import settings
 app_name = "djangoapp"
 
 urlpatterns = [
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_user, name='logout'),
-    path(route='register', view=views.register_user, name='register'),
+    path(route='login', view=views.login_user, name='login'), 
+    path(route='logout', view=views.logout_user, name='logout'), 
+    path(route='register', view=views.register_user, name='register'),  
     path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships,
          name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
+         name='dealer_details')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
